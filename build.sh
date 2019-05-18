36e96249a5b907a471443746548b1aa31ad53ca2
Johannes Schüth <git@jotschi.de>
2019-05-18 11:26:13 +0200

    Remove native-image support

--- build.sh
+++ build.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-if [ -z "$GRAALVM_HOME" ] ; then
-  echo "Please set GRAALVM_HOME to point to your graalvm installation"
-  exit
-fi
-
 PROJECT_DIR="`dirname \"$0\"`"
-
 cd $PROJECT_DIR
 
-if [ "$1" == "native" ] ; then
-  ./mvnw clean package -DskipTests -Pnative
-  docker build -f Dockerfile.native -t gentics/mesh-alexa-skill-demo:native .
-else
-  ./mvnw clean package -DskipTests -Pshaded
-  docker build -t gentics/mesh-alexa-skill-demo:latest .
-fi
+./mvnw clean package -DskipTests
+docker build -t gentics/mesh-alexa-skill-demo:latest .
+
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-./target/mesh-alexa-skill-server  -Djavax.net.ssl.trustStore=/opt/jvm/graalvm/jre/lib/security/cacerts -Djavax.net.ssl.trustAnchors=/opt/jvm/graalvm/jre/lib/security/cacerts  -Djava.library.path=/opt/jvm/graalvm/jre/lib/amd64/
